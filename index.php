a5b8bd145ba3deb9d9418059b3fc26ce42fc0d43
dhawal r khatiwala <drk25@afsconnect1.njit.edu>
2016-09-29 13:35:08 -0400

    added some sample code

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+
+
+//this is how u print text
  echo 'hello world';
+ //line break
+ echo '<br>';
+//declare var
+ $myvar = 'Some Text i want to store in a variable';
+//diff between ' and "
+ echo '$myvar';
+ echo '<br>';
+ echo "$myvar";
+
+ 
  ?>
