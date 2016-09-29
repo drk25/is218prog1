f8cf6e73e3054f9be485c6c9d729decb547fb950
dhawal r khatiwala <drk25@afsconnect1.njit.edu>
2016-09-29 14:28:07 -0400

    add class types and object

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  //line break
  echo '<br>';
 //declare var
- $myvar = 'Some Text i want to store in a variable';
+ $myvar = 'Some Text i want to store in a variable <br>';
 //diff between ' and "
  echo '$myvar';
  echo '<br>';
@@ -33,9 +33,48 @@ echo '<br>';
 print_r($myAssoc);
 
 
+// to print out array of specified value of array
+print_r($myAssoc ['value1']);
 
+// this is how to define a class in php
+class myclass{
+// a class has properties
+// 3 properties
 
+//public are external to use properties outside of the class
+// can use out side the class
+public $myPublic;
 
+// private is hidden and only useable for myPrivate
+//cannot use outside the class
+private $myPrivate;
+
+// in between public and private but cannot use outside the class
+protected $myProtected;
+
+// the construct is method inside a class then method
+// outside the class then function
+public function __construct() {
+//this is the internal refrence to the object within a class
+$this->myPublic = 1;
+$this->myprivate = 2;
+$this->myProtected = 3;
+
+// this is how u call a method and pass it to value/var
+$this->sayHello('Dan');
+}
+//method
+public function sayHello($name){
+echo 'Hello' .$name;
+
+}
+// this is how you instanciated(take the class put and turn it into an obj) an object
+
+// a class is a scamatics(blueprint)to an obj
+  }
+  $obj = new myclass;
+
+  print_r($obj);
 
 
 
